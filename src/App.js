afc90311dd8c545b96544bc34e57bc6c66ad0048
Brandon Hanlon <give101k@gmail.com>
2020-01-20 01:46:48 -0500

    changed title

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import React from "react";
 import "./App.css";
 import github from "./images/GitHub-Mark-Light-120px-plus.png";
 import Countdown from "./countdown";
+import { TitleComponent } from "./TitleComponent.jsx";
 
 function App() {
   return (
-    <div className="bgimg">
-      <div className="middle">
-        <h1>COMING SOON</h1>
-        <hr />
-        <Countdown />
-        <hr />
-        <a href="https://github.com/give101k">
-          <img src={github} alt="" id="github" />
-        </a>
+    <>
+      <TitleComponent />
+
+      <div className="bgimg">
+        <div className="middle">
+          <h1>COMING SOON</h1>
+          <hr />
+          <Countdown />
+          <hr />
+          <a href="https://github.com/give101k">
+            <img src={github} alt="" id="github" />
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
