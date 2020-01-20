6bfca56a560fc7746715ea90707178cbf2f22cb8
Brandon Hanlon <give101k@gmail.com>
2020-01-20 01:03:58 -0500

    Updates

---
+++ precache-manifest.e8da9d912e247a37478f34a1817a9e15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50cb0342de56f579629578a9d36c1587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf6e7b5915671e0da072",
+    "url": "/static/css/main.d7b041c7.chunk.css"
+  },
+  {
+    "revision": "e286e090210c7f5caceb",
+    "url": "/static/js/2.a1f03a9d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.a1f03a9d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cf6e7b5915671e0da072",
+    "url": "/static/js/main.e47cf5f1.chunk.js"
+  },
+  {
+    "revision": "c9913191e565de632244",
+    "url": "/static/js/runtime-main.170f8412.js"
+  },
+  {
+    "revision": "c7c0a44d3fd5d2f31439e903d81b5f29",
+    "url": "/static/media/background.c7c0a44d.jpeg"
+  }
+]);
