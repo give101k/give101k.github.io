bbe90a4e19935f4c526868e225ec6c87b6de3b9d
Brandon Hanlon <give101k@gmail.com>
2020-02-03 12:58:56 -0500

    changed date

--- src/countdown.js
+++ src/countdown.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 export default class Countdown extends Component {
   state = {
-    countdate: new Date("Jan 27, 2020 0:0:0").getTime(),
+    countdate: new Date("Feb 10, 2020 0:0:0").getTime(),
     days: 0,
     hours: 0,
     minutes: 0,
