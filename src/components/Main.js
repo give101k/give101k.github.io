33b9fa7128376eba6656d76b4fe5585340ef89b1
Brandon Hanlon <give101k@gmail.com>
2020-09-01 14:18:07 -0400

    added and started the project component

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,13 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Home";
 import Cv from "./Cv";
+import Project from "./Project";
+
 const Main = () => (
   <main className="page landing-page">
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/projects" component={Project} />
       <Route exact path="/cv" component={Cv} />
     </Switch>
   </main>
---
+++ src/components/Project.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+function Project() {
+  return (<div>hello</div>);
+}
+export default Project;
